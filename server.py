a1e9879beaad0cd93f2fef85b1c83e3c05bea3a5
Bryan-Armas10 <bryan.armas@sdgku.edu>
2024-11-09 13:53:02 -0800

    assignment3

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
@@ -18,7 +18,7 @@ def text():
 
 # endPoint using json
 app.get("/api/about")
-def abouutGet():
+def aboutGet():
     name = {"name": "Adrian"}
     return json.dumps(name)
 
@@ -35,5 +35,47 @@ def greet(name):
 def farewell(name):
     return f"""
     <h1 style=color:blue>bye bye {name}!</h1>"""
+
+# ###############################################
+products = []
+@app.get("/api/products")
+def get_products():
+    return json.dumps(products)
+
+@app.post("/api/products")
+def save_products():
+    item = request.get_json()
+    print(item)
+    products.append(item)
+    return json.dumps(item)
+
+@app.put("/api/products/<int:index>")
+def update_products(index):
+    updated_item = request.get_json()
+    if 0 <= index <= len(products):
+        products[index] = updated_item
+        return json.dumps(updated_item)
+    else:
+        return "That index does not exist"
+
+@app.delete("/api/products/<int:index>")
+def delete_products(index):
+    delete_item = request.get_json()
+    if 0 <= index <= len(products):
+        delete_products = products.pop(index)
+        return json.dumps(delete_item)
+    else:
+        return "That index does not exist"
+
+# pacth -- the method to update an especific element  into python is: list.update
+
+@app.patch("/api/products/<int:index>")
+def patch_products(index):
+    updated_field = request.get_json()
+    if 0 <= index <= len(products):
+        updated_field(index).update(updated_field)
+        return json.dumps(updated_field)
+    else:
+        return "That index does not exist"
 
 app.run(debug=True)
